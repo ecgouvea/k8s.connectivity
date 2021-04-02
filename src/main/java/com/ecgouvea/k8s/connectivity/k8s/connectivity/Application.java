9578d06f069bcb3d5d47621cbfe90ca149f1cb42
Eduardo Gouvea <ecgouvea@hotmail.com>
2021-04-02 16:55:00 -0300

    Initial commit

---
+++ src/main/java/com/ecgouvea/k8s/connectivity/k8s/connectivity/Application.java
@@ -0,0 +1,13 @@
+package com.ecgouvea.k8s.connectivity.k8s.connectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
